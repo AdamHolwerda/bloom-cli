c559a8f736e851215f2bf796163f677312b1561a
AdamHolwerda <adamholwerda@gmail.com>
2020-05-05 12:58:07 -0400

    add action to run tests

--- test/toWebTitle.test.js
+++ test/toWebTitle.test.js
@@ -3,11 +3,14 @@ const expect = chai.expect;
 
 const toWebTitle = require('../dist/funcs/toWebTitle');
 
-describe('Converts a title to a web title', () => {
-    it('converts a title to a web title', () => {
-        const a = toWebTitle('This Is A Title That Should Become A Web Title');
-        expect(a).to.equal(
-            'this-is-a-title-that-should-become-a-web-titlef8089122be5b4236ffa00655f2c5a8c1'
-        );
-    });
+describe('Converts a title to a web title', function(){
+	it('converts a title to a web title', function () {
+		const a = toWebTitle('This Is A Title That Should Become A Web Title');
+		expect(a).to.equal('this-is-a-title-that-should-become-a-web-titlef8089122be5b4236ffa00655f2c5a8c1');
+	});
+
+	it('converts a title with characters to a web title', function () {
+		const a = toWebTitle('This: Is! A. Title-That? Should% Become A Web Title');
+		expect(a).to.equal('this-is-a-title-that-should-become-a-web-title7c9853cd5da583037a49796ae3262e3f');
+	});
 });
